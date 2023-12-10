0cb34a9ae71fe0e1d3d597bae582988720692442
Oriz Wahyu N <oriezt.id@gmail.com>
2023-12-10 16:36:46 +0700

    add update contact api

--- tests/Feature/ContactTest.php
+++ tests/Feature/ContactTest.php
@@ -129,4 +129,59 @@ class ContactTest extends TestCase
                 ]
             ]);
     }
+
+    public function testUpdateSuccess()
+    {
+        $this->seed([UserSeeder::class, ContactSeeder::class]);
+
+        $contact = Contact::query()->limit(1)->first();
+
+        $this->put(
+            '/api/contacts/' . $contact->id,
+            [
+                'first_name' => 'test2',
+                'last_name' => 'test2',
+                'email' => 'test2@test.test',
+                'phone' => '0857582'
+            ],
+            [
+                'Authorization' => 'test'
+            ]
+        )->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    'first_name' => 'test2',
+                    'last_name' => 'test2',
+                    'email' => 'test2@test.test',
+                    'phone' => '0857582'
+                ]
+            ]);
+    }
+
+    public function testUpdateValidationError()
+    {
+        $this->seed([UserSeeder::class, ContactSeeder::class]);
+
+        $contact = Contact::query()->limit(1)->first();
+
+        $this->put(
+            '/api/contacts/' . $contact->id,
+            [
+                'first_name' => '',
+                'last_name' => 'test2',
+                'email' => 'test2@test.test',
+                'phone' => '0857582'
+            ],
+            [
+                'Authorization' => 'test'
+            ]
+        )->assertStatus(400)
+            ->assertJson([
+                'errors' => [
+                    'first_name' => [
+                        'The first name field is required.'
+                    ]
+                ]
+            ]);
+    }
 }

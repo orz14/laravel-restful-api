db509ddb1bcb0ca3346419eb433c1fa7fe01ad38
Oriz Wahyu N <oriezt.id@gmail.com>
2023-12-10 14:39:12 +0700

    add create contact api

---
+++ tests/Feature/ContactTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Feature;
+
+use Database\Seeders\UserSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ContactTest extends TestCase
+{
+    public function testCreateSuccess()
+    {
+        $this->seed([UserSeeder::class]);
+
+        $this->post('/api/contacts', [
+            'first_name' => 'Oriz',
+            'last_name' => 'Wahyu N',
+            'email' => 'admin@orzproject.dev',
+            'phone' => '081228390894'
+        ], [
+            'Authorization' => 'test'
+        ])->assertStatus(201)
+            ->assertJson([
+                'data' => [
+                    'first_name' => 'Oriz',
+                    'last_name' => 'Wahyu N',
+                    'email' => 'admin@orzproject.dev',
+                    'phone' => '081228390894'
+                ]
+            ]);
+    }
+
+    public function testCreateFailed()
+    {
+        $this->seed([UserSeeder::class]);
+
+        $this->post('/api/contacts', [
+            'first_name' => '',
+            'last_name' => 'Wahyu N',
+            'email' => 'admin',
+            'phone' => '081228390894'
+        ], [
+            'Authorization' => 'test'
+        ])->assertStatus(400)
+            ->assertJson([
+                'errors' => [
+                    'first_name' => [
+                        'The first name field is required.'
+                    ],
+                    'email' => [
+                        'The email field must be a valid email address.'
+                    ]
+                ]
+            ]);
+    }
+
+    public function testCreateUnauthorized()
+    {
+        $this->seed([UserSeeder::class]);
+
+        $this->post('/api/contacts', [
+            'first_name' => '',
+            'last_name' => 'Wahyu N',
+            'email' => 'admin',
+            'phone' => '081228390894'
+        ], [
+            'Authorization' => 'salah'
+        ])->assertStatus(401)
+            ->assertJson([
+                'errors' => [
+                    'message' => [
+                        'Unauthorized'
+                    ]
+                ]
+            ]);
+    }
+}
